b3aeca77605c4bad6cab91f2c6b39cde4efd87b7
Nuga <93000491+nuga-dev@users.noreply.github.com>
2022-06-05 13:14:32 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'nuga#5889'
+description 'A NoPixel Inspired Notification System Made For QBCore'
+version 'Release'
+
+client_script 'functions.lua'
+
+ui_page {
+    'html/ui.html',
+}
+
+files {
+	'html/ui.html',
+	'html/js/app.js', 
+	'html/css/style.css',
+}
+
+lua54 'yes'
